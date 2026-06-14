b56e05aa363e91b9c57d113c56ec0ba39f02a8c9
Stefanos <stefanospaiste@gmail.com>
2026-06-14 21:10:04 +0300

    Home page html with styling and API integration

---
+++ javascript/javascript.js
@@ -0,0 +1,21 @@
+const quoteElement = $("#quote");
+const authorElement = $("#author");
+const buttonElement = $("#new-quote");
+
+async function fetchquote() {
+    const url = "https://dummyjson.com/quotes/random";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        const result = await response.json();
+        quoteElement.text(result.quote);
+        authorElement.text(result.author);
+        console.log(result);
+    } catch (error) {
+        console.error("Error fetching quote:", error);
+    }
+}
+buttonElement.on("click", fetchquote);
