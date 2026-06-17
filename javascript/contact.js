ddf4baf4cbe6e11d1b2562c784feb925cf9c306c
Stefanos <stefanospaiste@gmail.com>
2026-06-18 01:32:54 +0300

    Navbar and Footer injections. Finished contact page. Report and analytics page also toggle light mode WIP

---
+++ javascript/contact.js
@@ -0,0 +1,34 @@
+$(document).ready(function (){
+    $("#contactForm").on("submit", function (event) {
+        event.preventDefault();
+
+        const name=$("#contactName").val().trim();
+        const email=$("#contactEmail").val().trim();
+        const message=$("#contactMessage").val().trim();
+
+        if (name.length > 50){
+            alert("Name cannot exceed 50 characters");
+            return;
+        }
+        if (message.length > 250){
+            alert("Name cannot exceed 250 characters");
+            return;
+        }
+        if (email.length > 50){
+            alert("Email cannot exceed 50 characters");
+            return;
+        }
+
+        if(name===""||email===""||message==="") {
+            alert("Fill all fields");
+            return;
+        }
+
+        $("#confirmationText").html(`Thank you, <strong> ${name}</strong> for your message.`);
+
+    const modal =new bootstrap.Modal($("#confirmationModal"));
+    modal.show();
+
+    $("#contactForm")[0].reset();
+    });
+});
